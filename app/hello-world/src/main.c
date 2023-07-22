d7dbc0c23ab4d55e70b3d7e5d687e140d2ef0b79
Chris Cabang <ccscabang@gmail.com>
2023-07-23 02:26:06 +0800

    feat: riscv sample hello world application

--- app/hello-world/src/main.c
+++ app/hello-world/src/main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ *  DEPENDS: sudo apt install qemu-user-static
+ *  $> ${TOOLCHAIN}/riscv${XLEN}-linux-gcc -static -o hello main.c
+ *  $> qemu-riscv${XLEN}-static hello
+ */
+
 int main() {
     printf("Hello World!\n");
     return 0;
