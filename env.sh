5a26b5a2cd74c1bb1d4508aa84f5deae5230fe89
Chris Cabang <ccscabang@gmail.com>
2023-07-23 02:31:08 +0800

    feat: env variables

---
+++ env.sh
@@ -0,0 +1,4 @@
+export TOOLCHAIN=/home/chris/Projects/embedded-linux-on-riscv/toolchain/riscv64-buildroot-linux-gnu_sdk-buildroot
+export PATH=$TOOLCHAIN/bin:$PATH
+export CROSS_COMPILE=riscv64-linux-
+export ARCH=riscv
